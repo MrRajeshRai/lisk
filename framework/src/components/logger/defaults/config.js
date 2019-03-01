08b0bc706475c631613aa1edc05e4c4f128f62bd
Nazar Hussain <nazar@lightcurve.io>
2019-03-01 10:40:52 +0100

    Add env, arg support for logger component

--- framework/src/components/logger/defaults/config.js
+++ framework/src/components/logger/defaults/config.js
@@ -4,14 +4,18 @@ const DefaultConfig = {
 		fileLogLevel: {
 			type: 'string',
 			default: 'info',
+			env: 'LISK_FILE_LOG_LEVEL',
+			arg: '-l,--log',
 		},
 		logFileName: {
 			type: 'string',
 			default: 'logs/lisk.log',
+			env: 'LISK_REDIS_HOST',
 		},
 		consoleLogLevel: {
 			type: 'string',
 			default: 'none',
+			env: 'LISK_CONSOLE_LOG_LEVEL',
 		},
 	},
 	required: ['fileLogLevel', 'logFileName', 'consoleLogLevel'],
