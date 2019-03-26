8cc3610b824fbb6a4f757dfe82b0626c46abeab3
Ishan <ishantiw.quasar@gmail.com>
2019-03-26 18:15:35 +0100

    Use params instead of filters that is modified from previous filter function

--- framework/src/modules/http_api/controllers/peers.js
+++ framework/src/modules/http_api/controllers/peers.js
@@ -69,8 +69,8 @@ PeersController.getPeers = async function(context, next) {
 		return next(null, {
 			data: peersByFilters,
 			meta: {
-				offset: filters.offset,
-				limit: filters.limit,
+				offset: params.offset.value,
+				limit: params.limit.value,
 				count: peersCount,
 			},
 		});
